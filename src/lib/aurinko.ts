d587b7b8336d4590e59da631da7ddd233ac547b8
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2026-02-21 22:41:25 +0530

    OAuth changes

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -29,7 +29,7 @@ export const getAurinkoAuthUrl = async (
     clientId: process.env.AURINKO_CLIENT_ID!,
     serviceType,
     responseType: "code",
-    returnUrl: `${process.env.NEXT_PUBLIC_URL}/oauth/intermediate`,
+    returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`,
     prompt: "consent",
     scopes: aurinkoScopes,
   });
